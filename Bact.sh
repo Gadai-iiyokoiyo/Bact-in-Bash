f2202e2797d69cf46099ca16e8e10a683830525f
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 18:56:14 +0900

    Update and rename Bact_in_Ubuntu.sh to Bact.sh

---
+++ Bact.sh
@@ -0,0 +1,14 @@
+while true
+do
+    read -p "Bact In Ubuntu 1.0 #" COMMAND
+    
+    if [ $COMMAND = exit ]; then
+        exit
+    elif [ $COMMAND = ubact_derivation1 ]; then
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact.sh" -o "Bact.sh"
+        sh Bact
+    elif [ $COMMAND = ubact_main ]; then
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh" -o "Bact.sh"
+    fi
+
+done
