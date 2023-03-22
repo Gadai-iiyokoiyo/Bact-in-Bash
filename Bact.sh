1cd6a907d00c09c3cb85b5979562e51402651626
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 19:22:57 +0900

    Update Bact.sh

--- Bact.sh
+++ Bact.sh
@@ -9,7 +9,7 @@ do
     elif [ $COMMAND = ubact_main ]; then
         wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact.sh" -O "Bact.sh"
     elif [ $COMMAND = ubact_derivation_other ]; then
-        read -p "derivation_name:" $nm
+        read -p "derivation_name:" nm
         wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/$nm/Bact.sh" -O "Bact.sh"
     elif [ $COMMAND = pget ]; then
         read -p "url:" $nm
