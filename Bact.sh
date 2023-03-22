0697b54ef7c8312d7c3a5f1a93d85fc040ae5732
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 18:59:00 +0900

    Update Bact.sh

--- Bact.sh
+++ Bact.sh
@@ -1,11 +1,11 @@
 while true
 do
-    read -p "Bact In Ubuntu 1.0 #" COMMAND
+    read -p "Bact In Ubuntu 1.0 : " COMMAND
     
-    if [ $COMMAND = exit ]; then
+    if [ $COMMAND = exits ]; then
         exit
     elif [ $COMMAND = ubact_derivation1 ]; then
-        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact.sh" -o "Bact.sh"
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/OD1/Bact.sh" -o "Bact.sh"
         sh Bact
     elif [ $COMMAND = ubact_main ]; then
         wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh" -o "Bact.sh"
