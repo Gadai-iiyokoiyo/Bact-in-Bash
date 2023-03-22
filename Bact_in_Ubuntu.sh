f0506929be3d13fa36ab0d1f42c79ed0f2694fbb
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 18:56:38 +0900

    Update Bact_in_Ubuntu.sh

--- Bact_in_Ubuntu.sh
+++ Bact_in_Ubuntu.sh
@@ -1,10 +1,14 @@
 while true
 do
     read -p "Bact In Ubuntu 1.0 #" COMMAND
-    if [ $COMMAND = ubact ]; then
-        echo "RoCom権限が必要です 以下のコマンドを実行してください: RoCom ubact"
-    elif [ $COMMAND = RoCom_ubact ]; then
-        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh"
+    
+    if [ $COMMAND = exits ]; then
+        exit
+    elif [ $COMMAND = ubact_derivation1 ]; then
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact.sh" -o "Bact.sh"
+        sh Bact
+    elif [ $COMMAND = ubact_main ]; then
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh" -o "Bact.sh"
     fi
 
 done
