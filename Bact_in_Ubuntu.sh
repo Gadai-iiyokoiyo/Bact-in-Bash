771ba52715b8f11612925d30bd4904d710e0b6c0
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 19:01:29 +0900

    Update Bact_in_Ubuntu.sh

--- Bact_in_Ubuntu.sh
+++ Bact_in_Ubuntu.sh
@@ -6,7 +6,6 @@ do
         exit
     elif [ $COMMAND = ubact_derivation1 ]; then
         wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/OD1/Bact.sh" -o "Bact.sh"
-        sh Bact
     elif [ $COMMAND = ubact_main ]; then
         wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh" -o "Bact.sh"
     fi
