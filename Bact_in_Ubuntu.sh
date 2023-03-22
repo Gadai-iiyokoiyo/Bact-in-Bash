8bbef1ea1d68272c11a8ef0f080e215301241ba3
Gadai-iiyokoiyo <121931749+Gadai-iiyokoiyo@users.noreply.github.com>
2023-03-22 18:33:05 +0900

    Update Bact_in_Ubuntu.sh

--- Bact_in_Ubuntu.sh
+++ Bact_in_Ubuntu.sh
@@ -1,15 +1,10 @@
 while true
 do
     read -p "Bact In Ubuntu 1.0 #" COMMAND
-    if [$COMMAND == "ubact"] then
+    if [ $COMMAND = ubact ]; then
         echo "RoCom権限が必要です 以下のコマンドを実行してください: RoCom ubact"
-    elif [$COMMAND == "RoCom ubact"] then
-        $RoCom_pass = "RoCom-$RANDOM"
-        echo "確認として以下の文字列を打ち込んでください:$RoCom_pass"
-        read -p ">" RoCom_Cpass
-        if [$RoCom_Cpass==$RoCom_pass] then
-            echo "TEST"
-        fi
+    elif [ $COMMAND = RoCom_ubact ]; then
+        wget "https://raw.githubusercontent.com/Gadai-iiyokoiyo/Bact-in-Bash/main/Bact_in_Ubuntu.sh"
     fi
 
 done
